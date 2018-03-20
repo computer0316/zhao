aa63646e4dd6570be877ad3af892ff0a131be72b
Roc <computer0316@126.com>
2018-03-20 08:55:26 +0800

    video

--- js/index.js
+++ js/index.js
@@ -30,6 +30,23 @@
 			setAnimation("s5Box4", "zoomIn");
 			setAnimation("s5Box5", "zoomIn");
 			
+			setAnimation("videoTitle", "zoomIn");
+			setAnimation("videoSub", "zoomIn");
+			setAnimation("video1", "zoomIn");
+			setAnimation("video2", "zoomIn");
+			setAnimation("video3", "zoomIn");
+			setAnimation("video4", "zoomIn");
+			setAnimation("video5", "zoomIn");
+			setAnimation("video6", "zoomIn");
+			setAnimation("video7", "zoomIn");
+			setAnimation("video8", "zoomIn");
+			setAnimation("video9", "zoomIn");
+			setAnimation("video10", "zoomIn");
+			setAnimation("video11", "zoomIn");
+			setAnimation("video12", "zoomIn");
+			setAnimation("video13", "zoomIn");
+			setAnimation("video14", "zoomIn");
+			
 			setAnimation("s6p1", "zoomIn");
 			setAnimation("s6p2", "zoomIn");
 			setAnimation("s6p3", "zoomIn");
