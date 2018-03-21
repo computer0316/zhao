0f4a203439e2bad61bd04540ee9f57af551672f4
Roc <computer0316@126.com>
2018-03-21 15:44:51 +0800

    last vision

--- js/index.js
+++ js/index.js
@@ -1,45 +1,17 @@
 	   window.addEventListener('scroll',function(){
-            setAnimation("s1Title", "zoomIn");
-            setAnimation("s1Line", "zoomIn");
-
-            setAnimation("leftTop2", "zoomIn");
-            setAnimation("rightTop2", "zoomIn");
-            setAnimation("leftBottom2", "zoomIn");
-            setAnimation("rightBottom2", "zoomIn");
-
-            setAnimation("leftTop1", "zoomIn");
-            setAnimation("rightTop1", "zoomIn");
-            setAnimation("leftBottom1", "zoomIn");
-            setAnimation("rightBottom1", "zoomIn");
-            
-            setAnimation("centerBox", "zoomIn");
-            
-			
-			setAnimation("s4Title", "zoomIn");
-            setAnimation("s4Line", "zoomIn");
-            setAnimation("s4Img1", "zoomIn");
-            setAnimation("s4Img2", "zoomIn");
-            setAnimation("s4Img3", "zoomIn");
-            
-            setAnimation("s5Title", "zoomIn");
-            setAnimation("s5Line", "zoomIn");
-
-			setAnimation("s5Box1", "zoomIn");
-			setAnimation("s5Box2", "zoomIn");
-			setAnimation("s5Box3", "zoomIn");
-			setAnimation("s5Box4", "zoomIn");
-			setAnimation("s5Box5", "zoomIn");
-			
+ 			
 			setAnimation("videoTitle", "zoomIn");
 			setAnimation("videoSub", "zoomIn");
 			setAnimation("video1", "zoomIn");
 			setAnimation("video2", "zoomIn");
 			setAnimation("video3", "zoomIn");
 			setAnimation("video4", "zoomIn");
+			
 			setAnimation("video5", "zoomIn");
 			setAnimation("video6", "zoomIn");
 			setAnimation("video7", "zoomIn");
 			setAnimation("video8", "zoomIn");
+			
 			setAnimation("video9", "zoomIn");
 			setAnimation("video10", "zoomIn");
 			setAnimation("video11", "zoomIn");
@@ -47,12 +19,22 @@
 			setAnimation("video13", "zoomIn");
 			setAnimation("video14", "zoomIn");
 			
-			setAnimation("s6p1", "zoomIn");
-			setAnimation("s6p2", "zoomIn");
-			setAnimation("s6p3", "zoomIn");
-			setAnimation("s6p4", "zoomIn");
+			setAnimation("item1", "fadeInLeft");
+			setAnimation("item2", "fadeInUp");
+			setAnimation("item3", "fadeInRight");
+			setAnimation("item4", "fadeInLeft");
+			setAnimation("item5", "fadeInUp");
+			setAnimation("item6", "fadeInRight");
+			setAnimation("item7", "fadeInLeft");
+			setAnimation("item8", "fadeInUp");
+			setAnimation("item9", "fadeInRight");
+			setAnimation("item10", "fadeInLeft");
+			setAnimation("item11", "fadeInUp");
+
 			
 			setAnimation("s3Title", "zoomIn");
 			setAnimation("s3Sub", "zoomIn");
-			setAnimation("s5Box2", "zoomIn");
+			setAnimation("para1", "slideInLeft");
+			setAnimation("para2", "slideInRight");
+			setAnimation("para3", "slideInLeft");
        })
